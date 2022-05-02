cd99976615d7035cc77f71cc58088b39cc9b2488
Elisee-des <esabidani@gmail.com>
2022-05-02 11:18:37 +0000

    Gestion du Header fin

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import HeaderInfos from './components/HeaderInfos';
+
 
 const App = () => {
   return (
-    <div>
-      
+    <div className="app-container">
+      <header>
+        <HeaderInfos />
+      </header>
     </div>
   );
 };
